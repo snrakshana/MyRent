d34d5cf749ffb094b1114cc7237f95c4432bd6a4
Rakshana <nagarakshana.sridharan@gmail.com>
2020-08-31 17:15:01 +0530

    rakshana commit

--- personal/views.py
+++ personal/views.py
@@ -3,11 +3,19 @@ from account.models import User
 
 from advertisement.models import ADPost
 
+from django.core.paginator import Paginator
+
 def index(request):
-    users = User.objects.all()
+    # users = User.objects.all()
+
     ads = ADPost.objects.all()
+
+    paginator = Paginator(ads, 8)
+    page = request.GET.get('page')
+    
+    ads = paginator.get_page(page)
+    
     context = {
-        'users':users,
         'ads':ads
     }
     return render(request,'personal/index.html',context)
